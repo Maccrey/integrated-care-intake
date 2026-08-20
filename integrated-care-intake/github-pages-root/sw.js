9ab6781b39216a763c33039d2adcc5965ba102c0
Seok hoon Lim <m01071630214@gmail.com>
2026-08-20 10:45:51 +0900

    first commit

---
+++ integrated-care-intake/github-pages-root/sw.js
@@ -0,0 +1,23 @@
+const CACHE='integrated-care-pwa-v3';
+const ASSETS=['./','./index.html','./style.css','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin) return;
+  event.respondWith(caches.match(event.request).then(cached=>{
+    const network=fetch(event.request).then(resp=>{
+      const copy=resp.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      return resp;
+    }).catch(()=>cached);
+    return cached || network;
+  }));
+});
